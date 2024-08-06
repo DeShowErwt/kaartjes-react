088f45f737586b42b1344ff4fbafecedf8a4822c
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-08-07 00:50:03 +0200

    Geef opties voor de squares

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,17 @@ export default function App() {
     const maps = config.maps
     const [mapValue, setMapValue] = useState(0)
     const [previousMapValue, setPreviousMapValue] = useState(0)
-
-    // const sizing = Math.floor(100 / maps.length)
+    const [outsideSquare, setOutsideSquare] = useState(true)
 
     function handleClick(index: number) {
         setPreviousMapValue(mapValue)
         setMapValue(index)
     }
 
+
     return (
         <>
+            <button onClick={() => setOutsideSquare(!outsideSquare)} className="absolute top-2 left-[100px] bg-[#213547] text-[rgba(255,255,255,0.87)] p-2">Verschillende square opties</button> 
             <Map header={maps[mapValue].header} year={parseInt(maps[mapValue].year)} description={maps[mapValue].description} image={maps[mapValue].image} previousMap={previousMapValue} /> 
             <div className="flex self-center text-center w-full my-4 h-12 border-2 border-black bg-[rgba(255,255,255,0.95)]">
                 {maps.map((map, index) => {
@@ -34,7 +35,7 @@ export default function App() {
                             )}
                         >
                             <p className={
-                                "translate-y-[150%] text-[#213547]"
+                                outsideSquare ? "translate-y-[150%] text-[#213547]" : ''
                             }> 
                                 { map.year }
                             </p>

d48d8edf097f5bce3b8be1cbe89cc4430bf31fa3
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-08-08 10:23:23 +0200

    Animated progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Map from './components/Map.tsx'
 import config from '../config.json'
 import clsx from 'clsx'
@@ -15,12 +15,27 @@ export default function App() {
         setMapValue(index)
     }
 
+    const barRef = useRef<HTMLDivElement>(null)
+    const progressRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (barRef.current && progressRef.current) {
+            progressRef.current.style.left = barRef.current.offsetLeft + 'px'
+            progressRef.current.style.top = barRef.current.offsetTop + 'px'
+        }
+    }, [barRef.current, progressRef.current])
+
+    useLayoutEffect(() => {
+        if (barRef.current && progressRef.current) {
+            progressRef.current.style.width = ((barRef.current.clientWidth/5) * (mapValue + 1)) + 'px'
+        }
+    }, [mapValue])
 
     return (
         <>
             <button onClick={() => setOutsideSquare(!outsideSquare)} className="absolute top-2 left-[100px] bg-[#213547] text-[rgba(255,255,255,0.87)] p-2">Verschillende square opties</button> 
             <Map header={maps[mapValue].header} year={parseInt(maps[mapValue].year)} description={maps[mapValue].description} image={maps[mapValue].image} previousMap={previousMapValue} /> 
-            <div className="flex self-center text-center w-full my-4 h-12 border-2 border-black bg-[rgba(255,255,255,0.95)]">
+            <div ref={barRef} className="flex self-center text-center w-full my-4 h-12 border-2 border-black bg-[rgba(255,255,255,0.95)]">
                 {maps.map((map, index) => {
                     return (
                         <button 
@@ -28,9 +43,8 @@ export default function App() {
                             data-year={map.year}
                             onClick={() => handleClick(index)} 
                             className={clsx(
-                                "col-span-1 border-r-2 border-black px-4 w-[20%]",
-                                styles.square,
-                                index <= mapValue ? 'bg-[#213547] text-[rgba(255,255,255,0.87)]' : '', 
+                                "col-span-1 border-r-2 z-10 border-black px-4 w-[20%]",
+                                styles.square, 
                                 index === maps.length - 1 ? 'border-r-0' : ''
                             )}
                         >
@@ -42,6 +56,7 @@ export default function App() {
                         </button>
                     )
                 })}
+                <div className="absolute top-0 left-0 h-12 bg-[#213547] border-2 border-black border-r-0 transition-all duration-1000 ease-out" ref={progressRef} />
             </div>
         </>
     )

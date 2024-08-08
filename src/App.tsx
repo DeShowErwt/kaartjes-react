948fdcccc66b2bb33ffc94caefee4ee09198d53f
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-08-08 10:54:48 +0200

    Looks normal on resize and remove button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import Map from './components/Map.tsx'
 import config from '../config.json'
 import clsx from 'clsx'
@@ -8,7 +8,6 @@ export default function App() {
     const maps = config.maps
     const [mapValue, setMapValue] = useState(0)
     const [previousMapValue, setPreviousMapValue] = useState(0)
-    const [outsideSquare, setOutsideSquare] = useState(true)
 
     function handleClick(index: number) {
         setPreviousMapValue(mapValue)
@@ -18,24 +17,27 @@ export default function App() {
     const barRef = useRef<HTMLDivElement>(null)
     const progressRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
+    const handleWindowResize = () => {
         if (barRef.current && progressRef.current) {
-            progressRef.current.style.left = barRef.current.offsetLeft + 'px'
-            progressRef.current.style.top = barRef.current.offsetTop + 'px'
+            progressRef.current.classList.add(styles.notransition)
+            progressRef.current.style.width = ((barRef.current.clientWidth/5) * (mapValue + 1)) + 'px'
+            // Random read to force reflow
+            progressRef.current.offsetHeight
+            progressRef.current.classList.remove(styles.notransition)
         }
-    }, [barRef.current, progressRef.current])
+    }
 
     useLayoutEffect(() => {
         if (barRef.current && progressRef.current) {
             progressRef.current.style.width = ((barRef.current.clientWidth/5) * (mapValue + 1)) + 'px'
         }
+        window.addEventListener('resize', handleWindowResize)
     }, [mapValue])
 
     return (
         <>
-            <button onClick={() => setOutsideSquare(!outsideSquare)} className="absolute top-2 left-[100px] bg-[#213547] text-[rgba(255,255,255,0.87)] p-2">Verschillende square opties</button> 
             <Map header={maps[mapValue].header} year={parseInt(maps[mapValue].year)} description={maps[mapValue].description} image={maps[mapValue].image} previousMap={previousMapValue} /> 
-            <div ref={barRef} className="flex self-center text-center w-full my-4 h-12 border-2 border-black bg-[rgba(255,255,255,0.95)]">
+            <div ref={barRef} className="flex relative self-center text-center w-full my-4 h-12 border-2 border-black bg-[rgba(255,255,255,0.95)]">
                 {maps.map((map, index) => {
                     return (
                         <button 
@@ -48,15 +50,13 @@ export default function App() {
                                 index === maps.length - 1 ? 'border-r-0' : ''
                             )}
                         >
-                            <p className={
-                                outsideSquare ? "translate-y-[150%] text-[#213547]" : ''
-                            }> 
+                            <p className="translate-y-[150%] text-[#213547]"> 
                                 { map.year }
                             </p>
                         </button>
                     )
                 })}
-                <div className="absolute top-0 left-0 h-12 bg-[#213547] border-2 border-black border-r-0 transition-all duration-1000 ease-out" ref={progressRef} />
+                <div className={clsx("absolute h-12 bg-[#213547] border-2 border-black border-r-0 duration-1000 ease-out -top-0.5 -left-0.5")} ref={progressRef} />  
             </div>
         </>
     )

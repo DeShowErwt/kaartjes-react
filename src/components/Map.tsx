748e6183b515d26423f142d397b864f389cdb227
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-08-09 11:04:54 +0200

    Parser function idea

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef } from 'react'
 import config from '../../config.json'
 import Counter from './Counter'
 
@@ -13,12 +14,32 @@ export default function MapComponent(props: MapProps) {
     const maps = config.maps
     const previousYear = parseInt(maps[props.previousMap].year)
 
+    const contentRef = useRef<HTMLDivElement>(null)
+
+    const returnDiv = document.createElement('div')
+
+    function parseEnters(base : string) {
+        returnDiv.innerHTML = ''
+        const paragraphArray = base.split('{./.}')
+        for (let i = 0; i < paragraphArray.length; i++) {
+            const paragraph = paragraphArray[i]
+            returnDiv.innerHTML += `${paragraph}<br>`
+        }
+        if(contentRef.current){
+            contentRef.current.innerHTML = returnDiv.innerHTML
+        }
+    }
+
+    useLayoutEffect(() => {
+        parseEnters(props.description)
+    })
+
     return (
         <div className="grid grid-cols-3 gap-10">
             <img className="col-span-2" src={props.image} alt="Map" />
             <div>
                 <h1 className="text-center mb-8">{ props.header } (ca. <Counter from={previousYear} to={props.year} />)</h1> 
-                <p>{ props.description }</p>
+                <p ref={contentRef} />
             </div>
         </div>
     )
